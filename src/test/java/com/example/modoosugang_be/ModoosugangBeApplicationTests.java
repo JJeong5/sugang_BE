76d3a2f5bf36f7212092dd2312c34dd42d532b9e
JJungMe <ojh4917@naver.com>
2024-01-03 11:41:34 +0900

    @SpringBootTest 주석 처리

--- src/test/java/com/example/modoosugang_be/ModoosugangBeApplicationTests.java
+++ src/test/java/com/example/modoosugang_be/ModoosugangBeApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.modoosugang_be;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ModoosugangBeApplicationTests {
 
     @Test
