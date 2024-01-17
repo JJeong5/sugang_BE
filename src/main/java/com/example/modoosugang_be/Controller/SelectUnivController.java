cffaf7f53086cc1de496b19dfab72e27ae6c0770
JJungMe <ojh4917@naver.com>
2024-01-17 15:27:03 +0900

    대학 리스트 post 코드 수정, cors 설정 수정

--- src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
+++ src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
@@ -3,27 +3,23 @@ package com.example.modoosugang_be.Controller;
 import com.example.modoosugang_be.Domain.University;
 import com.example.modoosugang_be.Service.UniversityService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
-//@RequestMapping(value = "/api")
+@RequestMapping(value = "/api")
 @RequiredArgsConstructor
 public class SelectUnivController {
 
     private final UniversityService universityService;
 
 
-    @RestController
-    public class univ_list {
 
         @PostMapping("/home/univ")
         public ArrayList<University> test1() {
 
             return universityService.findUniversity();
         }
-    }
 
 }
